ca73a0facc2d7ab3869e31a393ffbac804d29f4d
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2023-08-18 18:31:41 +0300

    Add files via upload

---
+++ shadowos.ps1
@@ -0,0 +1,20 @@
+# Got code from https://github.com/lstprjct/IDM-Activation-Script, Thank You lstprjct and credits to you
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/ShadowElixir/ShadowOS/main/ShadowOS/shadowos.bat'
+
+$FilePath = "$env:TEMP\shadowos.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
